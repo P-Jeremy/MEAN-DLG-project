7d975e57e4c59c01afa9f25b38db55fa085e3496
D0C702WH0 <jemyplu@gmail.com>
2019-03-21 12:18:42 +0100

    server connected to client

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Subject } from 'rxjs';
 
 /* POST interface */
@@ -13,9 +14,14 @@ export class PostsService {
   private posts: Post[] = [];
   private postUpdated = new Subject<Post[]>();
 
+  constructor(private http: HttpClient) {}
 
   getPosts() {
-    return [...this.posts];
+    this.http.get<{message: string, posts: Post[]}>('http://localhost:8080/api/posts')
+        .subscribe((postData) => {
+          this.posts = postData.posts;
+          this.postUpdated.next([...this.posts]);
+        });
   }
 
   getPostUpdatedListener() {
@@ -24,6 +30,7 @@ export class PostsService {
 
   addPosts(title: string, content: string) {
     const post: Post = {
+      id: null,
       title,
       content
     };
