ef7fe73ff53a018163cd48f115f949d3885bff4a
D0C702WH0 <jemyplu@gmail.com>
2019-03-22 10:47:07 +0100

    Mongoose setup

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,40 @@
 const express = require('express');
+const cors = require('cors')
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const mongoConf = process.env.MONGO_CONFIG_URL;
+
+const Post = require('./models/post');
 
 const app = express();
 
+mongoose.connect(mongoConf, { useNewUrlParser: true })
+  .then(() => {
+    console.log("Connected to DB!");
+  })
+  .catch(() => {
+    console.log(" Unable to connect to DB...");
+  })
+
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
-  next();
-});
+/* MANUAL CORS */
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
+//   next();
+// });
 
 app.post('/api/posts', (req, res, next) => {
-  const posts = req.body
-  console.log(posts);
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
+  post.save();
   res.status(201).json({
     message: "Posts added"
   });
