d8535a8d55e887778ef395d5396fd5f7a21e2859
D0C702WH0 <jemyplu@gmail.com>
2019-04-11 16:22:31 +0200

    song title filterPipe

--- src/app/songs/song.module.ts
+++ src/app/songs/song.module.ts
@@ -1,19 +1,22 @@
 import { NgModule } from '@angular/core';
 import { SongCreateComponent } from './song-create/song-create.component';
 import { SongListComponent } from './song-list/song-list.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { AngularMaterialModule } from '../angular-material.module';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FilterTitlePipe } from './filter-title.pipe';
 
 
 @NgModule({
   declarations: [
-    SongCreateComponent,
+    FilterTitlePipe,
     SongListComponent,
+    SongCreateComponent,
   ],
   imports: [
     CommonModule,
+    FormsModule,
     ReactiveFormsModule,
     AngularMaterialModule,
     RouterModule
