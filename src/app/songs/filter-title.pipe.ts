dab1bc418a97435db1d14a955026179f7e853297
D0C702WH0 <jemyplu@gmail.com>
2019-04-14 17:19:20 +0200

    Asc pipe improvments

--- src/app/songs/filter-title.pipe.ts
+++ src/app/songs/filter-title.pipe.ts
@@ -6,14 +6,22 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class FilterTitlePipe implements PipeTransform {
 
   transform(songs: any, term: any, isTitle: boolean): any {
-    if (term === undefined) { return songs; }
+    if (term === undefined) {
+      return songs.sort((a, b) => {
+        return a.title === b.title ? 0 : a.title < b.title ? -1 : 1;
+      });
+    }
     if (isTitle) {
       return songs.filter((song) => {
         return song.title.toLowerCase().includes(term.toLowerCase());
+      }).sort((a, b) => {
+        return a.title === b.title ? 0 : a.title < b.title ? -1 : 1;
       });
     } else {
       return songs.filter((song) => {
         return song.author.toLowerCase().includes(term.toLowerCase());
+      }).sort((a, b) => {
+        return a.author === b.author ? 0 : a.author < b.author ? -1 : 1;
       });
     }
   }
