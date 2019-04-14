12cb408a01420d3b05d11928ce5bfee402d0e8ab
D0C702WH0 <jemyplu@gmail.com>
2019-04-14 19:40:48 +0200

    Better reset password logic

--- backend/models/post.js
+++ backend/models/post.js
@@ -4,19 +4,19 @@ const commentSchema  = mongoose.Schema({
   content: {type: String, required: true},
   creator_id : {type: String, required: true},
   creator_pseudo: {type: String, required: true},
-  createdAt: { type: Date, default: Date.now, required: false },
-  updatedAt: { type: Date, default: Date.now, required: false },
 })
+
+commentSchema.set('timestamps', true);
 
 const postSchema = mongoose.Schema({
   title: {type: String, required: true},
   content: {type: String, required: true},
   image: {type: String || null},
-  createdAt: { type: Date, default: Date.now, required: false },
-  updatedAt: { type: Date, default: Date.now, required: false },
   comments: [commentSchema],
   creator_id: {type: mongoose.Schema.Types.ObjectId, ref: "User", required:true},
   creator_pseudo: {type: String, ref: "User", required:true}
 });
+
+postSchema.set('timestamps', true);
 
 module.exports = mongoose.model('Post', postSchema);
