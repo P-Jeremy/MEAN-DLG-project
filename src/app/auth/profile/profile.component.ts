5458a27be8d87f649e0f6f5bf078176c4b157bd2
D0C702WH0 <jemyplu@gmail.com>
2019-04-15 17:26:53 +0200

    fetch user infos ok

--- src/app/auth/profile/profile.component.ts
+++ src/app/auth/profile/profile.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
+
+
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  private userId: string;
+  userData: {};
+
+  constructor(public route: ActivatedRoute, public authService: AuthService) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe((paramMap: ParamMap) => {
+      this.userId = paramMap.get('userId');
+      this.userData = this.authService.getUserData(this.userId).subscribe((data) => {
+        this.userData = data.data;
+      });
+    });
+
   }
 
 }
