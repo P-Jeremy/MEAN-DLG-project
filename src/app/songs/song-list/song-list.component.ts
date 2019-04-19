983a76cae8c8ec8523201ac231556a60741667fa
D0C702WH0 <jemyplu@gmail.com>
2019-04-19 12:22:46 +0200

    General styling, new spinner

--- src/app/songs/song-list/song-list.component.ts
+++ src/app/songs/song-list/song-list.component.ts
@@ -32,29 +32,32 @@ export class SongListComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     this.isLoading = true;
-    this.songsService.getSongs(this.songsPerPage, this.currentPage);
-    this.songSub = this.songsService.getSongUpdatedListener()
-      .subscribe((songData: { songs: Song[], songCount: number }) => {
-        this.totalSongs = songData.songCount;
-        this.songs = songData.songs;
+    setTimeout(() => {
+      this.songsService.getSongs(this.songsPerPage, this.currentPage);
+      this.songSub = this.songsService.getSongUpdatedListener()
+        .subscribe((songData: { songs: Song[], songCount: number }) => {
+          this.totalSongs = songData.songCount;
+          this.songs = songData.songs;
+        });
+      this.userIsAdmin = this.authService.getIsAdmin();
+      this.adminListenerSub = this.authService
+        .getAdminStatusListener()
+        .subscribe(isUserAdmin => {
+          this.userIsAdmin = isUserAdmin;
+        });
+      this.route.paramMap.subscribe((paramMap: ParamMap) => {
+        this.isLoading = true;
+        if (paramMap.has('shuffle')) {
+          this.isShuffle = true;
+          setTimeout(() => {
+            this.onShuffle();
+            this.isLoading = false;
+          }, 200);
+        }
+        this.isLoading = false;
       });
-    this.userIsAdmin = this.authService.getIsAdmin();
-    this.adminListenerSub = this.authService
-      .getAdminStatusListener()
-      .subscribe(isUserAdmin => {
-        this.userIsAdmin = isUserAdmin;
-      });
-    this.route.paramMap.subscribe((paramMap: ParamMap) => {
-      this.isLoading = true;
-      if (paramMap.has('shuffle')) {
-        this.isShuffle = true;
-        setTimeout(() => {
-          this.onShuffle();
-          this.isLoading = false;
-        }, 200);
-      }
-      this.isLoading = false;
-    });
+    }, 500);
+
   }
 
   onChangedPage(pageData: PageEvent) {
