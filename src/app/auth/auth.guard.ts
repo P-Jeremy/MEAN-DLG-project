1684fd65eb3b6338294decd5091d35d6cfd04c9c
D0C702WH0 <jemyplu@gmail.com>
2019-04-20 17:22:21 +0200

    Post notification

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -19,7 +19,7 @@ export class AuthGuard implements CanActivate {
   ): boolean | Observable<boolean> | Promise<boolean> {
     const isAuth = this.authService.getIsAuth();
     if (!isAuth) {
-      this.authService.redirect(['/']);
+      this.authService.logout();
     }
     return isAuth;
   }
