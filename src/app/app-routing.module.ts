2ffdda5febc064ccd90c110058f00a098b8dc3b1
D0C702WH0 <jemyplu@gmail.com>
2019-05-07 10:32:05 +0200

    Posts lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './auth/auth.guard';
 import { AdminGuard } from './auth/admin.guard';
 
-
-import { PostListComponent } from './posts/post-list/post-list.component';
-import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { SongCreateComponent } from './songs/song-create/song-create.component';
 import { SongListComponent } from './songs/song-list/song-list.component';
 import { HomeComponent } from './home/home.component';
@@ -13,13 +10,11 @@ import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
-  { path: 'post', component: PostListComponent, canActivate: [AuthGuard] },
-  { path: 'create_post', component: PostCreateComponent, canActivate: [AuthGuard] },
-  { path: 'edit_post/:postId', component: PostCreateComponent, canActivate: [AuthGuard] },
   { path: 'song', component: SongListComponent, canActivate: [AuthGuard] },
   { path: 'song/:shuffle', component: SongListComponent, canActivate: [AuthGuard] },
   { path: 'create_song', component: SongCreateComponent, canActivate: [AdminGuard] },
   { path: 'edit_song/:songId', component: SongCreateComponent, canActivate: [AdminGuard] },
+  { path: 'post', loadChildren: './posts/post.module#PostModule' },
   { path: 'auth', loadChildren: './auth/auth.module#AuthModule' }
 ];
 
