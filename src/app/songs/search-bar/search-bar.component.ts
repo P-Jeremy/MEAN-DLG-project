3ba50e044efb73099370a354f8e7851c7ca7b204
D0C702WH0 <jemyplu@gmail.com>
2019-05-09 17:19:38 +0200

    Spinner and background styling

--- src/app/songs/search-bar/search-bar.component.ts
+++ src/app/songs/search-bar/search-bar.component.ts
@@ -4,7 +4,7 @@ import { SearchBarService } from '../../services/search-bar.service';
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
-  styleUrls: ['./search-bar.component.css']
+  styleUrls: ['./search-bar.component.scss']
 })
 export class SearchBarComponent {
 
