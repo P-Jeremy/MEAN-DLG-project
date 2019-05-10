4b944b3ceeb0610310028c21483266323f44b603
D0C702WH0 <jemyplu@gmail.com>
2019-05-10 09:46:44 +0200

    General background height fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { SwUpdate } from '@angular/service-worker';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
   constructor(private authService: AuthService, private swUpdate: SwUpdate) { }
